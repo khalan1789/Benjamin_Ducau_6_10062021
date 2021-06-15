1c4dd4e8dce7b3339041510db4efba9a6296197a
Ducau Benjamin <ducaubenjamin@gmail.com>
2021-06-15 17:58:58 +0200

    mise en place du .env

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const dotenv = require("dotenv").config();
 const mongoose = require("mongoose");
 // const path = require('path');
 
-const Sauce = require('./models/sauce');
+
+const URL_PATH = process.env.URL_PATH; 
+const sauceRoutes = require('./routes/sauce');
+const auth = require('./routes/user'); //rajouté avant de couper
 
 const app = express();
 
-mongoose.connect('mongodb+srv://Benjamin_Ducau:!Solymar37!@cluster0.vaqax.mongodb.net/Piquante?retryWrites=true&w=majority',
+mongoose.connect(process.env.URL_PATH,
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à la bdd MongoDB réussie !'))
@@ -22,72 +26,10 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 
-app.post('/api/sauces',(req, res, next) => {
-  delete req.body._id;
-  const sauce = new Sauce({
-    ...req.body
-  });
-  sauce.save()
-    .then(res.status(201).json({ message : "sauce créée avec succès !"}))
-    .catch(error => res.status(400).json({ error}))
-  });
-
-app.put('/api/sauces/:id',(req, res, next) => {
-  Sauce.updateOne({_id : req.params.id}, {...req.body, _id : req.params.id})
-    .then(sauces => res.status(200).json({message : "sauce modifiée"}))
-    .catch(error => res.status(400).json({ error}))
-});
-
-app.delete('/api/sauces/:id',(req, res, next) => {
-  Sauce.deleteOne({_id : req.params.id})
-    .then(sauce => res.status(200).json({message : "sauce supprimée !"}))
-    .catch(error => res.status(400).json({ error}))
-});
-
-app.get('/api/sauces/:id',(req, res, next) => {
-    Sauce.findOne({_id : req.params.id})
-      .then(sauce => res.status(200).json(sauce))
-      .catch(error => res.status(400).json({ error}))
-  });
-
-app.get('/api/sauces',(req, res, next) => {
-  Sauce.find()
-    .then(sauces => res.status(200).json({sauces}))
-    .catch(error => res.status(400).json({ error}))
-});
-
-
-
-
-
-
-
-
-// app.post('/api/sauces',(req, res,next) => {
-//   console.log(req.body);
-//   res.status(201).json({
-//     message: 'Objet créé !'
-//   });
-  
-// });
-
-// app.get('/api/sauces',(req, res,next) => {
-//   data.find()
-//   .then(console.log(data))
-//   console.log(res.json(data))
-//   // Sauce.find()
-//   //   .then(sauces => res.status(208).json({sauces}))
-//   //   .catch(error => res.status(404).json({error}))
-// });
-
-// app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use('/api/sauces', sauceRoutes);
+app.use('/api/auth', auth);
 
 
 
 module.exports = app;
 
-
-//lien de connexion mongoDB au cas ou
-//en version myfristdatabase
-
-// mongodb+srv://Benjamin_Ducau:<password>@cluster0.vaqax.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
