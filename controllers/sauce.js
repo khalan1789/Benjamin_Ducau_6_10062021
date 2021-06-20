a57aa35a009e8105d8a02eb4728abb60447296a0
Ducau Benjamin <ducaubenjamin@gmail.com>
2021-06-20 11:03:56 +0200

    premières mises en place pour test like

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -1,6 +1,7 @@
 
 const Sauce = require('../models/sauce');
 const fs = require('fs');
+const sauce = require('../models/sauce');
 
 exports.createSauce = (req, res, next) => {
     const sauceObject = JSON.parse(req.body.sauce);
@@ -45,8 +46,7 @@ exports.deleteSauce = (req, res, next) => {
           .catch(error => res.status(400).json({ error}));
         })
       })
-      .catch(error => res.status(500).json({ error }));
-    
+      .catch(error => res.status(500).json({ error }));    
 };
 
 exports.getAllSauce = (req, res, next) => {
@@ -56,61 +56,80 @@ exports.getAllSauce = (req, res, next) => {
 };
 
  // système de like
- exports.voteSauce = (req, res, next) => {
-  Sauce.findOne({_id : req.params.id})
-    .then(sauce => {
-      console.log(sauce)
-      console.log(req.body)
-      res.status(201).json({ message : "i know you like it"})
-    })
-    .catch(error => res.status(405).json({message : "errrrrrreeeeuur"}))
 
-  // console.log(req.body)
-  // res.status(666).json({message : "test du like !"})
- /*const vote = req.body.like;
-    const sauce = req.body;
-      if (vote = 1){
-        // if(!sauce.usersLiked[userId]){
-          Sauce.updateOne({_id : req.params.id}, {
-           $push : { usersLiked : sauce.userId}
-          })
-            .then(() => res.status(201).json({message : "j'aime ajouté"}))
+ exports.voteSauce = (req, res, next) => {
+  console.log(req.body.userId )
+  const vote = req.body.like;
+  sauce.findOne({_id : req.params.id})
+  .then( sauce => {
+  switch(vote){
+    case "1" : //console.log("j'aime")
+    // res.status(201).json({message : "j'aime ok"})
+      Sauce.updateOne({_id : req.params.id}, {
+               $push : { usersLiked : req.body.userId}
+            }, {$inc : {likes : +1 }})
+            .then(() => res.status(201).json({message : "USER DE j'aime ajouté"}))
             .catch(error => res.status(500).json({ error}))
-            
-        // // }
-        // else{
-        //   console.log("l'utilisateur a déjà cliqué sur j'aime!")
-        // }
-      }
-      else if (vote = -1){
-        // if(!sauce.usersDislikes[userId]){
-          Sauce.updateOne({_id : req.params.id}, {
-            $push : { usersLiked : sauce.userId}
-           })
-             .then(() => res.status(201).json({message : "je n'aime pas ajouté"}))
-             .catch(error => res.status(500).json({ error}))
-             
-        // // }
-        // else{
-        //   console.log("l'utilisateur a déjà cliqué sur je n'aime pas...!")
-        // }
-      }
-      else if(vote = 0){
-        // supprimer l'id user du tableau dans lequel il était et mettre -1 dans le total de ce tableau là
-        // : array fonction mongoDb à voir.
-        Sauce.updateOne({_id : req.params.id}, {
-          $pull : { usersLiked : sauce.userId, usersDisliked : sauce.userId}
-         })
-           .then(() => res.status(201).json({message : "vote réinitialisé"}))
-           .catch(error => res.status(500).json({ error}))
-           
-        console.log("pas encore pret")
-      }
-    */
-    
-    // .catch( error => res.status(500).json({ error}))
+            break;
+    case "-1" : // console.log("j'aime pas")
+    // res.status(201).json({message : "j'aime pas c'est bon"})
+      Sauce.updateOne({_id : req.params.id}, {
+        $push : { usersDisliked : req.body.userId}, /*$inc : {dislikes : +1 }*/
+      })
+            .then(() => res.status(201).json({message : "je n'aime pas ajouté"}))
+            .catch(error => res.status(500).json({ error}))
+            break;   
+  }
+})
+  .catch(error => res.status(500).json({ message : "ça n'a pas swicthé"}))
 }
 
+//// NE PAS Y PRETER ATTENTION A LA PARTIE D'APRES :) ////
+//  exports.voteSauce = (req, res, next) => {
+//   Sauce.findOne({_id : req.params.id})
+//     .then(sauce => {
+//       const vote = req.body.like;
+//       switch(vote){
+//         case vote = 1 :
+//           // if(!sauce.usersLiked[req.params.userId]){
+//             Sauce.updateOne({_id : req.params.id}, {
+//                $push : { usersLiked : sauce.userId}, /* $inc : {likes : +1 } */
+//             })
+//             .then(() => res.status(201).json({message : "USER DE j'aime ajouté"}))
+//             .catch(error => res.status(500).json({ error}))
+              
+//           // }
+//           // else{
+//           //   console.log("l'utilisateur a déjà cliqué sur j'aime!")
+//           // };
+//           break;
+      
+//         case vote = -1 :
+//           // if(!sauce.usersDislikes[req.params.userId]){
+//             Sauce.updateOne({_id : req.params.id}, {
+//               $push : { usersDisliked : sauce.userId}, /* $inc : {likes : -1 }*/
+//             })
+//               .then(() => res.status(201).json({message : "je n'aime pas ajouté"}))
+//               .catch(error => res.status(500).json({ error}))   
+//           // }
+//           // else{
+//           //   console.log("l'utilisateur a déjà cliqué sur je n'aime pas...!")
+//           // }
+//           break;
+//         case vote = 0 :
+//           // supprimer l'id user du tableau dans lequel il était et mettre -1 dans le total de ce tableau là
+//           // : array fonction mongoDb à voir.
+//           Sauce.updateOne({_id : req.params.id}, {
+//              $pull : { usersLiked : sauce.userId, usersDisliked : sauce.userId}
+//           })
+//             .then(() => res.status(201).json({message : "vote réinitialisé"}))
+//             .catch(error => res.status(500).json({ error}))
+//           break;   
+//       };       
+//     })
+//     .catch(error => res.status(500).json({error : error}))
+// };
+  
 // if (vote = 1){
 //   if(il n'y a pas l'user dans le tableau des likes){
 //   //mettre la fonction pour ajouter l'id au tableau like
@@ -135,3 +154,20 @@ exports.getAllSauce = (req, res, next) => {
 //   supprimer l'id user du tableau dans lequel il était et mettre -1 dans ce tableau là
 //   : array fonction mongoDb à voir.
 // } 
+
+/****************AVANT DE TESTER LE SWITCH
+ * 
+ * 
+ *exports.voteSauce = (req, res, next) => {
+//Sauce.findOne({_id : req.params.id})
+ * then(sauce => {
+      console.log(sauce)
+      console.log(req.body)
+      res.status(201).json({ message : "i know you like it"})
+    })
+    .catch(error => res.status(405).json({message : "errrrrrreeeeuur"}))
+ * 
+ * 
+ * 
+ * 
+ */
